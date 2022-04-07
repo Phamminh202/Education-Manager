fc138264fa0ceed0a3245111ea3a6e561afceaa1
Phamminh202 <73330638+Phamminh202@users.noreply.github.com>
2022-04-07 22:44:30 +0700

    change_all

---
+++ EducationManager.Data/Models/Academicyear.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EducationManager.Data.Models
+{
+    public partial class Academicyear
+    {
+        public Academicyear()
+        {
+            Mark = new HashSet<Mark>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Mark> Mark { get; set; }
+    }
+}
