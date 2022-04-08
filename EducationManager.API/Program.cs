d03858456316f1bcbd1f5c45666f2a016eaf13dd
Phamminh202 <73330638+Phamminh202@users.noreply.github.com>
2022-04-08 11:52:35 +0700

    change_all_2

--- EducationManager.API/Program.cs
+++ EducationManager.API/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EducationManager.API
+namespace EducationManager.Api
 {
     public class Program
     {
--- EducationManager.API/Startup.cs
+++ EducationManager.API/Startup.cs
@@ -1,17 +1,17 @@
-using EducationManager.Data.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EducationManager.API
+namespace EducationManager.Api
 {
     public class Startup
     {
@@ -25,12 +25,7 @@ namespace EducationManager.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // them ket noi db
-            var connectionString = Configuration.GetConnectionString("educationmanager");
-            services.AddDbContext<educationmanagerContext>(options => options.UseSqlServer(connectionString));
-
-            //
-            services.AddControllersWithViews();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,14 +35,8 @@ namespace EducationManager.API
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -55,9 +44,7 @@ namespace EducationManager.API
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
         }
     }
