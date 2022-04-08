4cf0e9b81473c74bd03a2772ffa26df58ce151d0
Đạt.Code <73332200+datyasuo1@users.noreply.github.com>
2022-04-08 16:13:36 +0700

    P2_Api

---
+++ EducationManager.API/Controllers/ExamsController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EducationManager.API.Models;
+
+namespace EducationManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamsController : ControllerBase
+    {
+        private readonly educationmanagerContext _context;
+
+        public ExamsController(educationmanagerContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Exams
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Exam>>> GetExams()
+        {
+            return await _context.Exams.ToListAsync();
+        }
+
+        // GET: api/Exams/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Exam>> GetCategory(int id)
+        {
+            var exam = await _context.Exams.FindAsync(id);
+
+            if (exam == null)
+            {
+                return NotFound();
+            }
+
+            return exam;
+        }
+
+        // PUT: api/Exams/5
+        // To protect from overposting attacks,see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutExam(int id, Exam exam)
+        {
+            if (id != exam.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(exam).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExamExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Exams
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Exam>> PostExam(Exam exam)
+        {
+            _context.Exams.Add(exam);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetExam", new { id = exam.Id }, exam);
+        }
+
+        // DELETE: api/Exams/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExam(int id)
+        {
+            var exam = await _context.Exams.FindAsync(id);
+            if (exam == null)
+            {
+                return NotFound();
+            }
+
+            _context.Exams.Remove(exam);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ExamExists(int id)
+        {
+            return _context.Exams.Any(e => e.Id == id);
+        }
+    }
+}
